9926ee9394f51c3973ad3017556375f62c27ca3b
wsw-black <2225180543@qq.com>
2021-12-16 09:09:29 +0800

    下载、删除、上传功能

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import paramiko
+import os
+import time
 
 
-# # 实例化SSHClient
-# ssh_client = paramiko.SSHClient()
-# # 自动添加策略，保存服务器的主机和密钥信息，如果不添加，那么不在本地know_hosts文件中记录的主机无法连接，此方法必须放在connect方法前面
-# ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# # 链接主机
-# ssh_client.connect(hostname="10.4.66.70", port=22, username='root', password='hbhl9500$')
-# # 打开一个channel(通信)并执行命令, 结果放到stdout中，如果有错误将放到stderr中
-# stdin, stdout, stderr = ssh_client.exec_command('df')
-# # 输出返回信息
-# stdout_info = stdout.read().decode('utf-8')
-# print(stdout_info)
-# # 输出返回错误信息
-# stderr_info = stderr.read().decode('utf-8')
-# print(stderr_info)
-# ssh_client.close()
 # 登录参数
 hostname = '10.4.66.70'
 host_port = 22
 username = 'root'
 password = 'hbhl9500$'
 
+
 def target_info():
     '''获取需要强关的文件路径和名称'''
+    global file_info
     file_info = input("请输入需要强关文件的路径:").strip()
+
 
 def ssh_client_con():
     """ 创建ssh并建立链接"""
@@ -50,17 +40,23 @@ def sftp_client_con():
     tran.connect(username=username, password=password)
     # 2. 创建sftp实例化
     sftp = paramiko.SFTPClient.from_transport(tran)
-    # 3. 执行上传功能
-    local_path = 'E:\\test.txt'    # 本地路径
-    remote_path = '/srv/dev-rbd0/技术部/王世伟/test.txt'    # 远程路径
-    # put_info = sftp.put(local_path, remote_path, confirm=True)
-    # print(put_info)
-    # print('上传%s完成'%local_path)
+    local_path = r'D:/test/2021年年中述职报告-王世伟.pptx'    # 本地路径
+    # remote_path = '/srv/dev-rbd0/技术部/王世伟/test.txt'    # 远程路径
+    # 下载共享上的文件到本地
+    font_path = r'/srv/dev-rbd0'
+    remote_path = font_path + file_info
+    print(remote_path)
+    sftp.get(remote_path, local_path)
+    print('下载%s完成'%remote_path)
+    # 删除共享上的文件
+    time.sleep(5)
     sftp.remove(remote_path)
-    print('%s已删除'%remote_path)
+    print("%s文件删除成功"%remote_path)
+    # 重新上传文件到服务器
+    put_info = sftp.put(local_path, remote_path, confirm=True)
+    print("%s上传成功"%local_path)
     # 关闭通道
     tran.close()
-
 
 
 target_info()
